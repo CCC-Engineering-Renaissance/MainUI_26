5e65ee1dec165cac8743097ee78b7e5c35574159
jlu <jus.lu67818@gmail.com>
2026-06-07 22:44:26 -0700

    fixed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QColor>
+#include <QPalette>
 #include <QSurfaceFormat>
 
 int main(int argc, char *argv[])
@@ -12,6 +14,29 @@ int main(int argc, char *argv[])
     QSurfaceFormat::setDefaultFormat(fmt);
 
     QApplication a(argc, argv);
+
+#ifdef Q_OS_LINUX
+    a.setStyle("Fusion");
+
+    QPalette dark;
+    dark.setColor(QPalette::Window,          QColor(0x0d, 0x1b, 0x2a));
+    dark.setColor(QPalette::WindowText,      Qt::white);
+    dark.setColor(QPalette::Base,            QColor(0x10, 0x1e, 0x2f));
+    dark.setColor(QPalette::AlternateBase,   QColor(0x16, 0x24, 0x36));
+    dark.setColor(QPalette::Text,            Qt::white);
+    dark.setColor(QPalette::Button,          QColor(0x16, 0x24, 0x36));
+    dark.setColor(QPalette::ButtonText,      Qt::white);
+    dark.setColor(QPalette::BrightText,      QColor(0x00, 0xcc, 0xff));
+    dark.setColor(QPalette::Highlight,       QColor(0x2c, 0xb5, 0xde));
+    dark.setColor(QPalette::HighlightedText, Qt::white);
+    dark.setColor(QPalette::ToolTipBase,     QColor(0x16, 0x24, 0x36));
+    dark.setColor(QPalette::ToolTipText,     Qt::white);
+    dark.setColor(QPalette::Link,            QColor(0x2c, 0xb5, 0xde));
+    dark.setColor(QPalette::Disabled, QPalette::ButtonText, QColor(0x80, 0x80, 0x80));
+    dark.setColor(QPalette::Disabled, QPalette::Text,       QColor(0x80, 0x80, 0x80));
+    a.setPalette(dark);
+#endif
+
     MainWindow w;
     w.show();
     return a.exec();
